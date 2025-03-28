24df09b6a2295de4de598aa6237fd0ab411a15ad
Saetbyeol Choi <sbyeol0520@gmail.com>
2025-03-28 10:49:53 -0700

    Fix .gitignore and keep google_drive_fetch.py tracked

--- extract/google_drive_fetch.py
+++ extract/google_drive_fetch.py
@@ -35,12 +35,7 @@ def list_csv_files(drive_service):
     # if not files:
     #     print("No CSV files found.")
     #     return
-    
-<<<<<<< HEAD
-    # print("\n📂 Found CSV Files in Google Drive:")
-=======
     # print("\n Found CSV Files in Google Drive:")
->>>>>>> origin/main
     # for file in files:
     #     print(f"- {file['name']} (ID: {file['id']})")
 
@@ -62,11 +57,7 @@ def download_file(drive_service, file_id, file_name):
     with open(file_path, "wb") as f:
         f.write(request.execute())  # Ensure file content is properly written
 
-<<<<<<< HEAD
-    print(f"✅ File downloaded: {file_path} ({os.path.getsize(file_path)} bytes)")
-=======
     print(f"File downloaded: {file_path} ({os.path.getsize(file_path)} bytes)")
->>>>>>> origin/main
     return file_path
 
 
@@ -74,11 +65,7 @@ def upload_to_s3(file_path, file_name):
     """Upload file to S3 bucket."""
     s3_key = f"raw/{file_name}"  # Save in 'raw' folder inside S3
     s3_client.upload_file(file_path, S3_BUCKET, s3_key)
-<<<<<<< HEAD
-    print(f"✅ Uploaded {file_name} to s3://{S3_BUCKET}/{s3_key}")
-=======
     print(f"Uploaded {file_name} to s3://{S3_BUCKET}/{s3_key}")
->>>>>>> origin/main
 
 def process_files():
     """Main function to download and upload CSV files."""
@@ -92,21 +79,13 @@ def process_files():
     for file in files:
         file_id = file['id']
         file_name = file['name']
-<<<<<<< HEAD
-        print(f"📥 Downloading: {file_name}")
-=======
         print(f"Downloading: {file_name}")
->>>>>>> origin/main
 
         file_path = download_file(drive_service, file_id, file_name)
         upload_to_s3(file_path, file_name)
         os.remove(file_path)  # Cleanup temporary file
 
-<<<<<<< HEAD
-    print("🎉 All files processed successfully.")
-=======
     print("All files processed successfully.")
->>>>>>> origin/main
 
 if __name__ == "__main__":
     process_files()
